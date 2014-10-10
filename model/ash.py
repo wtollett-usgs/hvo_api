03c4f9d279b58f504a955b2f9b9eb079cb9b178b
Bill Tollett <83-wtollett@users.noreply.code.usgs.gov>
2014-10-10 10:53:56 -1000

    Removed unnecessary import statements.

--- model/ash.py
+++ model/ash.py
@@ -1,17 +1,16 @@
-from sqlalchemy.ext.declarative import declared_attr
 from valverest.database import db
 from valverest.util import date_to_j2k
 
 class HMM(db.Model):
     __tablename__ = 'HMM'
     __bind_key__  = 'ash'
-    
+
     timestamp       = db.Column('j2ksec', db.Float, primary_key=True)
     accumrate       = db.Column(db.Float)
     percentjuvenile = db.Column(db.Float)
     tid             = db.Column(db.Integer)
     rid             = db.Column(db.Integer, primary_key=True)
-    
+
     def __init__(self, time='', ar='', pj=''):
         self.timestamp       = date_to_j2k(time, False)
         self.accumrate       = '%.3f' % float(ar) if ar != '' else None
