ebcd3778e300c01402fed70d1dcb73fe4ce5e6ab
Bill Tollett <wtollett-usgs@users.noreply.github.com>
2014-12-03 13:07:31 -1000

    Added GET for logsapi.

--- controller/logsapi.py
+++ controller/logsapi.py
@@ -1,12 +1,44 @@
 from datetime import datetime
-from flask import request
-from flask.ext.restful import Resource
+from flask import request, current_app
+from flask.ext.restful import Resource, reqparse
+from flask.ext.restful.representations.json import settings as json_settings
 from json import loads as jsonload
 from logging import getLogger
 from model import hvologs, cvologs, avologs
 from valverest.database import db2 as hvodb, db3 as cvodb, db4 as avodb
 
 class LogsAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('op', type = str, required = False, default = 'time')
+        self.reqparse.add_argument('observatory', type = str, required = False, default = 'hvo')
+        super(LogsAPI, self).__init__()
+
+    def get(self):
+        # Return expected parameter output, also set indent settings
+        if not request.args:
+            return self.create_param_string(), 200
+
+        if (not current_app.debug) and (json_settings and json_settings['indent']):
+            json_settings['indent'] = None
+
+        args = self.reqparse.parse_args()
+        if 'op' in args and args['op'] == 'time':
+            obs = args['observatory'].lower()
+            if obs == 'avo':
+                cname = avologs
+                db    = avodb
+            elif obs == 'cvo':
+                cname = cvologs
+                db    = cvodb
+            elif obs == 'hvo':
+                cname = hvologs
+                db    = hvodb
+
+            post = cname.Post.query.order_by(cname.Post.observtime.desc()).first()
+            return { 'id': post.obsID, 'observtime': post.observtime.strftime("%Y-%m-%d %H:%M:%S"),
+                     'obsdate': post.obsdate.strftime("%Y-%m-%d %H:%M:%S") }, 200
+
     def post(self):
         lf = getLogger('file')
         try:
@@ -28,8 +60,9 @@ class LogsAPI(Resource):
             item     = cname.Post(user.id, pdate, odate, arg['subject'], arg['text'], user.username)
             volcname = cname.ListVolc.query.filter_by(Volcano = arg['volcano']).first()
             volc     = cname.Volcano.query.filter_by(volcano_name = arg['volcano']).first()
-            lf.debug('Attempting to insert log entry for observatory=%s, date=%s, user=%s, subject=%s, text=%s'
-                    % (observatory, arg['date'], arg['user'], arg['subject'], arg['text']))
+            lf.debug(s = ('Attempting to insert log entry for observatory=%s, postdate=%s, obsdate=%s, user=%s, '
+                          'subject=%s, text=%s')
+                    % (observatory, arg['postdate'], arg['obsdate'], arg['user'], arg['subject'], arg['text']))
             db.session.add(item)
             db.session.commit()
             linkitem = cname.VolcLink(volcname.VolcNameID, item.obsID, volc.volcano_id)
@@ -41,3 +74,15 @@ class LogsAPI(Resource):
             return { 'status': 'ok' }, 201
         except:
             return { 'status': 'error' }, 400
+
+    @staticmethod
+    def create_param_string():
+        if not current_app.debug:
+            json_settings['indent']    = 4
+            json_settings['sort_keys'] = True
+
+        params = {}
+        params['observatory'] = { 'type': 'string', 'required': 'no', 'options': 'avo, cvo, hvo', 'default': 'hvo' }
+        params['op'] = { 'type': 'string', 'required': 'no', 'options': 'time',
+                         'note': 'Returns datetime for last record in the database. Other parameters not required.'}
+        return params
