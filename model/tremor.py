a78a92787b6138c02a4f16ce47d3604983aa4786
Bill Tollett <wtollett-usgs@users.noreply.github.com>
2015-01-26 14:32:36 -1000

    Further fleshed out tremor queries. Also added sql echo parameter to local config.

--- model/tremor.py
+++ model/tremor.py
@@ -1,4 +1,3 @@
-from sqlalchemy.ext.declarative import declared_attr
 from valverest.database import db
 
 class catalog(db.Model):
@@ -13,6 +12,31 @@ class catalog(db.Model):
     depth     = db.Column(db.Float)
     region    = db.Column(db.String(2))
     iseq      = db.Column(db.Integer)
+    stations  = db.relationship('station', backref='catalog')
 
     def __repr__(self):
     	return '%s, %f, %f, %f, region=%s, iseq=%d' % (self.starttime,self.latitude,self.longitude,self.depth, self.region, self.iseq)
+
+class station(db.Model):
+    __tablename__ = 'tremor_sta'
+    __bind_key__  = 'tremor'
+
+    evid      = db.Column(db.Integer, db.ForeignKey('tremor_catalog.evid'), primary_key=True)
+    sta       = db.Column(db.String(5))
+    chan      = db.Column(db.String(3))
+    #net       = db.Column(db.String(2))
+    #loc       = db.Column(db.String(2))
+    #envrms    = db.Column(db.Float)
+    #maxamp    = db.Column(db.Float)
+    #ampunits  = db.Column(db.String(10))
+    #peakFreq  = db.Column(db.Float)
+    #freqRatio = db.Column(db.Float)
+    #f1        = db.Column(db.Float)
+    #f2        = db.Column(db.Float)
+    staKey    = db.Column(db.Integer, primary_key=True)
+    chanKey   = db.Column(db.Integer, primary_key=True)
+    #rd        = db.Column(db.Float)
+    #weight    = db.Column(db.Float)
+
+    def __repr__(self):
+        return '%d, %s, %s' % (self.evid, self.sta, self.chan)
