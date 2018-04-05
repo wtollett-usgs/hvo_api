dd79b1c1735d6e7c65e7bbefdb6324496376a48d
Bill Tollett <83-wtollett@users.noreply.code.usgs.gov>
2018-04-04 16:18:31 -1000

    Added LaserLavaLevel datasource.

--- valverest/__init__.py
+++ valverest/__init__.py
@@ -8,16 +8,17 @@ from controller.flyspecapi import FlyspecAPI
 from controller.gpsapi import GPSAPI
 from controller.hypocenterapi import HypocenterAPI
 from controller.incompatiblesapi import IncompatiblesAPI
-from controller.mgosystematicsapi import MgOSystematicsAPI
-from controller.magmaticsulfurapi import MagmaticSulfurAPI
+from controller.laserlavalevelapi import LaserLavaLevelAPI
 from controller.lavalevelapi import LavaLevelAPI
 from controller.logsapi import LogsAPI
+from controller.mgosystematicsapi import MgOSystematicsAPI
+from controller.magmaticsulfurapi import MagmaticSulfurAPI
 from controller.rsamapi import RSAMAPI
 from controller.serviceapi import ServiceAPI
 from controller.strainapi import StrainAPI
 from controller.tiltapi import TiltAPI
 from controller.tremorapi import TremorAPI
-from valverest.database import db, db2, db3, db4, db5, db6
+from valverest.database import db, db2, db3, db4, db5, db6, db7, db8
 import logging
 
 def create_app(*args, **kwargs):
@@ -31,6 +32,8 @@ def create_app(*args, **kwargs):
     db4.init_app(app)
     db5.init_app(app)
     db6.init_app(app)
+    db7.init_app(app)
+    db8.init_app(app)
     api.add_resource(ServiceAPI, '/', '/api')
     api.add_resource(AshAPI, '/api/ash', endpoint = 'ash')
     api.add_resource(EDXRFAPI, '/api/edxrf', endpoint = 'edxrf')
@@ -39,6 +42,7 @@ def create_app(*args, **kwargs):
     api.add_resource(HypocenterAPI, '/api/hypocenter', endpoint = 'hypocenter')
     api.add_resource(IncompatiblesAPI, '/api/incompatibles', endpoint = 'incompatibles')
     api.add_resource(FileAPI, '/api/file', endpoint = 'file')
+    api.add_resource(LaserLavaLevelAPI, '/api/laserlavalevel', endpoint='laserlavalevel')
     api.add_resource(LavaLevelAPI, '/api/lavalevel', endpoint = 'lavalevel')
     api.add_resource(LogsAPI, '/api/logs', endpoint = 'logs')
     api.add_resource(MagmaticSulfurAPI, '/api/magmaticsulfur', endpoint = 'magmaticsulfur')
