a4b65c296310d5176938ef3c3cf551856fe1c075
Bill Tollett <83-wtollett@users.noreply.code.usgs.gov>
2018-06-20 13:02:02 -1000

    Added rtnet dataset.

---
+++ controller/rtnetapi.py
@@ -0,0 +1,117 @@
+from common_constants import MAX_LINES, DS_OPTIONS
+from flask import request, current_app
+from flask_restful import Resource, reqparse
+from valverest.database import db10 as db
+from valverest.util import create_date_from_input, date_to_j2k, j2k_to_date
+
+import model.rtnet as rtnet
+
+_series_options = ['north', 'northErr', 'east', 'eastErr', 'up', 'upErr', 
+                   'qual', 'numL1Amb', 'numSat', 'numL2Amb', 'tropDel',
+                   'tropErr']
+
+class RTNetAPI(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('channel', type = str, required = True)
+        self.reqparse.add_argument('rank', type = int, required = False, default = 4)
+        self.reqparse.add_argument('series', type = str, required = False, default = 'north,east,up')
+        self.reqparse.add_argument('starttime', type = str, required = True)
+        self.reqparse.add_argument('endtime', type = str, required = False, default = 'now')
+        self.reqparse.add_argument('timezone', type = str, required = False, default = 'hst')
+        self.reqparse.add_argument('debias', type = str, required = False, default = 'mean')
+        super(RTNetAPI, self).__init__()
+
+    def get(self):
+        if not request.args:
+            return self.create_param_string(), 200
+
+        if not current_app.debug:
+            current_app.config['RESTFUL_JSON'] = {}
+
+        args = self.reqparse.parse_args()
+
+        channels = args['channel'].split(',')
+        unknown  = set([x.upper() for x in channels]).difference(rtnet._tablenames)
+        if len(unknown) > 0:
+            return { 'Error': 'unknown channel(s): %s' % ','.join(unknown) }
+
+        series  = args['series'].split(',')
+        unknown = set([x.lower() for x in series]).difference(_series_options)
+        if len(unknown) > 0:
+            return { 'Error': 'unknown series: %s' % ','.join(unknown) }
+
+        tz         = (args['timezone'].lower() == 'hst')
+        start, end = create_date_from_input(args['starttime'], args['endtime'], tz)
+        output     = {}
+        count      = 0
+
+        for channel in channels:
+            queryclauses = []
+            orderby      = []
+            cname        = getattr(rtnet, channel.upper())
+
+            # Set up query filters
+            queryclauses.append(cname.timestamp.between(date_to_j2k(start, tz), date_to_j2k(end, tz)))
+
+            # Set up order by values
+            orderby.append(cname.timestamp.asc())
+
+            if args['rank'] == 0:
+                orderby.append(cname.rid.desc())
+            else:
+                queryclauses.append(cname.rid == args['rank'])
+
+            q = cname.query.filter(*queryclauses).order_by(*orderby)
+            try:
+                q = q.limit(MAX_LINES['RTNET'])
+            except KeyError:
+                pass
+            data = q.all()
+
+            output[channel] = []
+            Date            = j2k_to_date
+            List            = output[channel].append
+
+            # Means
+            means = {}
+            for i in series:
+                if args['debias'] == 'mean':
+                    means[i] = sum([getattr(x,i) for x in data]) / float(len(data)) if i in series else 0
+                else:
+                    means[i] = 0
+
+            for d in data:
+                a = { 'date': Date(d.timestamp, tz).strftime('%Y-%m-%d %H:%M:%S'),
+                      'rank': d.rank.name }
+
+                for i in series:
+                    a[i] = getattr(d, i) - means[i]
+
+                List(a)
+            count += len(data)
+        return { 'nr': count,
+                 'records': output }, 200
+
+    @staticmethod
+    def create_param_string():
+        if not current_app.debug:
+            settings = current_app.config.get('RESTFUL_JSON', {})
+            settings.setdefault('indent', 4)
+            settings.setdefault('sort_keys', True)
+            current_app.config['RESTFUL_JSON'] = settings
+
+        params = {}
+        params['channel'] = {'type': 'string', 'required': 'yes', 'note': 'Can be comma-separated list.',
+                            'options': rtnet._tablenames}
+        params['starttime'] = {'type': 'string', 'required': 'yes',
+                              'note': 'Will also accept things like -6m for last six months.',
+                              'format': 'yyyy[MMdd[hhmm]]'}
+        params['endtime'] = {'type': 'string', 'required': 'no', 'format': 'yyyy[MMdd[hhmm]]', 'default': 'now'}
+        params['rank'] = {'type': 'int', 'required': 'no', 'default': 4,
+                         'note': 'A rank of 0 will return the best possible rank.'}
+        params['timezone'] = {'type': 'string', 'required': 'no', 'default': 'hst'}
+        params['series'] = {'type': 'string', 'required': 'no', 'note': 'Can be comma-separated list.',
+                            'default': 'east,north,up', 'options': _series_options}
+        params['debias'] = {'type': 'string', 'required': 'no', 'default': 'mean', 'options': 'mean, none'}
+        return params
--- valverest/__init__.py
+++ valverest/__init__.py
@@ -11,16 +11,17 @@ from controller.incompatiblesapi import IncompatiblesAPI
 from controller.laserlavalevelapi import LaserLavaLevelAPI
 from controller.lavalevelapi import LavaLevelAPI
 from controller.logsapi import LogsAPI
-from controller.mgosystematicsapi import MgOSystematicsAPI
 from controller.magmaticsulfurapi import MagmaticSulfurAPI
+from controller.mgosystematicsapi import MgOSystematicsAPI
 from controller.rsamapi import RSAMAPI
+from controller.rtnetapi import RTNetAPI
 from controller.serviceapi import ServiceAPI
 from controller.so2highresapi import SO2HighResAPI
 from controller.strainapi import StrainAPI
 from controller.tiltapi import TiltAPI
-from controller.triggersapi import TriggersAPI
 from controller.tremorapi import TremorAPI
-from valverest.database import db, db2, db3, db4, db5, db6, db7, db8, db9
+from controller.triggersapi import TriggersAPI
+from valverest.database import db, db2, db3, db4, db5, db6, db7, db8, db9, db10
 import logging
 
 def create_app(*args, **kwargs):
@@ -37,24 +38,26 @@ def create_app(*args, **kwargs):
     db7.init_app(app)
     db8.init_app(app)
     db9.init_app(app)
+    db10.init_app(app)
     api.add_resource(ServiceAPI, '/', '/api')
     api.add_resource(AshAPI, '/api/ash', endpoint = 'ash')
     api.add_resource(EDXRFAPI, '/api/edxrf', endpoint = 'edxrf')
     api.add_resource(FlyspecAPI, '/api/flyspec', endpoint = 'flyspec')
+    api.add_resource(FileAPI, '/api/file', endpoint = 'file')
     api.add_resource(GPSAPI, '/api/gps', endpoint = 'gps')
     api.add_resource(HypocenterAPI, '/api/hypocenter', endpoint = 'hypocenter')
     api.add_resource(IncompatiblesAPI, '/api/incompatibles', endpoint = 'incompatibles')
-    api.add_resource(FileAPI, '/api/file', endpoint = 'file')
     api.add_resource(LaserLavaLevelAPI, '/api/laserlavalevel', endpoint='laserlavalevel')
     api.add_resource(LavaLevelAPI, '/api/lavalevel', endpoint = 'lavalevel')
     api.add_resource(LogsAPI, '/api/logs', endpoint = 'logs')
     api.add_resource(MagmaticSulfurAPI, '/api/magmaticsulfur', endpoint = 'magmaticsulfur')
     api.add_resource(MgOSystematicsAPI, '/api/mgosys', endpoint = 'mgosys')
     api.add_resource(RSAMAPI, '/api/rsam', endpoint = 'rsam')
+    api.add_resource(RTNetAPI, '/api/rtnet', endpoint = 'rtnet')
     api.add_resource(SO2HighResAPI, '/api/so2highres', endpoint = 'so2highres')
     api.add_resource(StrainAPI, '/api/strain', endpoint = 'strain')
     api.add_resource(TiltAPI, '/api/tilt', endpoint = 'tilt')
-    api.add_resource(TriggersAPI, '/api/triggers', endpoint = 'triggers')
     api.add_resource(TremorAPI, '/api/tremor', endpoint = 'tremor')
+    api.add_resource(TriggersAPI, '/api/triggers', endpoint = 'triggers')
     logging.basicConfig(filename='%s' % app.config['LOGFILE'], level=logging.DEBUG)
     return app
